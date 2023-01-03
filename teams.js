031a31c6ca10bf4a0eb6faee84ff164aec522fc2
Daniel Zamloot <45713834+danzam284@users.noreply.github.com>
2023-01-02 22:16:45 -0500

    Add files via upload

---
+++ teams.js
@@ -0,0 +1,186 @@
+var teamMap = {};
+teamMap["random"] = "random";
+teamMap["custom team"] = "custom";
+//Gen 4
+//Champion
+var cynthia = [34, "darkpulse", "psychic", "silverwind", "ominouswind", 22, "energyball", "sludgebomb", "shadowball", "extrasensory", 49, "airslash", "aurasphere", "waterpulse", "psychic", 15, "aurasphere", "dragonpulse", "psychic", "earthquake", 39, "surf", "icebeam", "dragonpulse", "swift", 17, "dragonrush", "earthquake", "brickbreak", "gigaimpact"];
+teamMap["cynthia"] = cynthia;
+//Elite 4
+var aaron = [143, "airslash", "bugbuzz", "x-scissor", "tackle", 54, "ironhead", "x-scissor", "nightslash", "tackle", 88, "attackorder", "airslash", "bugbuzz", "powergem", 26, "megahorn", "closecombat", "nightslash", "stoneedge", 36, "x-scissor", "crosspoison", "icefang", "aerialace", 147, "bugbuzz", "sludgebomb", "aerialace", "tacle"];
+teamMap["aaron"] = aaron;
+var bertha = [92, "earthpower", "aquatail", "zenheadbutt", "earthquake", 42, "earthquake", "icefang", "firefang", "thunderfang", 148, "earthquake", "stoneedge", "crunch", "bodyslam", 84, "earthquake", "firepunch", "thunderpunch", "doubleedge", 51, "earthquake", "rockwrecker", "megahorn", "avalanche", 95, "earthquake", "crunch", "brickbreak", "stoneedge"];
+teamMap["bertha"] = bertha;
+var flint = [50, "flamethrower", "sludgebomb", "darkpulse", "irontail", 47, "flamethrower", "gigaimpact", "tackle", "crunch", 94, "flareblitz", "solarbeam", "bounce", "flamethrower", 10, "flareblitz", "thunderpunch", "closecombat", "earthquake", 56, "flamethrower", "thunderbolt", "solarbeam", "hyperbeam", 28, "flamethrower", "ominouswind", "sludgebomb", "silverwind"];
+teamMap["flint"] = flint;
+var lucian = [96, "psychic", "thunderbolt", "shadowball", "focusblast", 101, "psychic", "shadowball", "tackle", "signalbeam", 32, "psychic", "ironhead", "earthquake", "zenheadbutt", 18, "psychic", "energyball", "focusblast", "swift", 44, "drainpunch", "psychocut", "leafblade", "stoneedge", 149, "psychic", "shadowball", "doublehit", "crunch"];
+teamMap["lucian"] = lucian;
+//Evil
+var cyrus = [50, "flamethrower", "darkpulse", "firefang", "thunderfang", 30, "drillpeck", "nightslash", "heatwave", "psychic", 23, "crosspoison", "airslash", "sludgebomb", "shadowball", 21, "waterfall", "icefang", "earthquake", "gigaimpact", 41, "nightslash", "icepunch", "x-scissor", "slash", 46, "thunderbolt", "irontail", "dig", "shadowball"];
+teamMap["cyrus"] = cyrus;
+//Extra
+var barry = [20, "bravebird", "tackle", "x-scissor", "closecombat", 89, "waterfall", "crunch", "icefang", "brickbreak", 22, "gigadrain", "energyball", "shadowball", "sludgebomb", 26, "megahorn", "stoneedge", "nightslash", "closecombat", 35, "gigaimpact", "zenheadbutt", "crunch", "earthquake", 10, "flareblitz", "aerialace", "shadowclaw", "focusblast"];
+teamMap["barry"] = barry;
+var cheryl = [150, "psychic", "tackle", "tackle", "tackle", 28, "swift", "sludgebomb", "ominouswind", "fly", 151, "stoneedge", "ironhead", "closecombat", "earthquake", 108, "avalanche", "earthquake", "hydropump", "brine", 97, "brickbreak", "doubleedge", "psychic", "shadowball", 19, "wildcharge", "crunch", "thunderbolt", "bite"];
+teamMap["cheryl"] = cheryl;
+var riley = [152, "doubleedge", "nightslash", "psychocut", "tackle", 132, "bodyslam", "avalanche", "shadowclaw", "aerialace", 71, "ironhead", "zenheadbutt", "brickbreak", "metalclaw", 70, "dragonclaw", "crunch", "firefang", "thunderfang", 15, "closecombat", "firepunch", "doubleedge", "earthquake", 27, "doublehit", "brickbreak", "poisonjab", "icepunch"];
+teamMap["riley"] = riley;
+var mira = [53, "triattack", "darkpulse", "psychic", "thunderbolt", 29, "shadowball", "energyball", "sludgebomb", "psychic", 16, "flashcannon", "hyperbeam", "shockwave", "thunder", 49, "aurasphere", "doubleedge", "psychic", "airslash", 18, "focusblast", "energyball", "psychic", "hyperbeam", 56, "flamethrower", "thunderbolt", "earthquake", "rockslide"];
+teamMap["mira"] = mira;
+var marley = [153, "bugbuzz", "aerialace", "x-scissor", "doubleedge", 139, "swift", "shockwave", "thunder", "hyperbeam", 23, "leechlife", "shadowclaw", "airslash", "poisonfang", 41, "icepunch", "aerialace", "nightslash", "brickbreak", 134, "flareblitz", "bodyslam", "thunderfang", "crunch", 81, "bravebird", "rockslide", "earthquake", "crunch"];
+teamMap["marley"] = marley;
+var buck = [131, "sludgebomb", "bodyslam", "rockslide", "tackle", 102, "darkpulse", "shadowball", "swift", "psychic", 154, "bodyslam", "flamethrower", "earthquake", "eruption", 52, "firepunch", "icepunch", "thunderpunch", "shadowball", 67, "psychic", "earthpower", "earthquake", "ancientpower", 85, "ironhead", "earthquake", "stoneedge", "dragonclaw"];
+teamMap["buck"] = buck;
+var elem;
+
+//Gen 3
+//Champion
+var steven = [64, "steelwing", "doubleedge", "aerialace", "bravebird", 67, "earthquake", "ancientpower", "shadowball", "psychic", 105, "earthquake", "dragonclaw", "thunder", "solarbeam", 114, "ancientpower", "shadowclaw", "sludgebomb", "energyball", 115, "ancientpower", "bodyslam", "aerialace", "waterpulse", 71, "ironhead", "earthquake", "psychic", "hyperbeam"];
+teamMap["steven"] = steven;
+//Elite 4
+var sidney = [155, "crunch", "doubleedge", "earthquake", "bite", 60, "darkpulse", "leafblade", "energyball", "extrasensory", 110, "needlearm", "solarbeam", "faintattack", "dig", 66, "nightslash", "surf", "waterfall", "slash", 152, "aerialace", "rockslide", "dig", "slash", 62, "surf", "crunch", "slash", "waterfall"];
+teamMap["sidney"] = sidney;
+var phoebe = [156, "shadowpunch", "shadowball", "earthquake", "bodyslam", 157, "shadowball", "shadowclaw", "flamethrower", "faintattack", 158, "shadowball", "slash", "shadowclaw", "faintattack", 157, "psychic", "shadowball", "thunderbolt", "slash", 156, "shadowball", "icebeam", "earthquake", "rockslide", 158, "shadowball", "darkpulse", "shadowclaw", "nightslash"];
+teamMap["phoebe"] = phoebe;
+var glacia = [159, "dig", "icebeam", "bodyslam", "blizzard", 160, "crunch", "icefang", "icebeam", "slash", 159, "blizzard", "doubleedge", "surf", "icebeam", 160, "doubleedge", "icebeam", "shadowball", "avalanche", 120, "surf", "icebeam", "bodyslam", "blizzard", 120, "surf", "icebeam", "bodyslam", "blizzard"];
+teamMap["glacia"] = glacia;
+var drake = [161, "rocktomb", "dragonclaw", "doubleedge", "shadowclaw", 111, "doubleedge", "dragonbreath", "aerialace", "airslash", 69, "dracometeor", "dragonclaw", "surf", "bodyslam", 65, "dragonbreath", "flamethrower", "crunch", "earthquake", 70, "dragonclaw", "flamethrower", "crunch", "rockslide", 161, "dragonclaw", "doubleedge", "bite", "rockslide"];
+teamMap["drake"] = drake;
+//Evil
+var maxie = [155, "crunch", "firefang", "thunderfang", "icefang", 23, "airslash", "crosspoison", "darkpulse", "sludgebomb", 171, "sludgebomb", "thunderbolt", "shadowball", "bodyslam", 63, "earthquake", "stoneedge", "flamethrower", "flashcannon", 172, "earthquake", "flamethrower", "ancientpower", "solarbeam", 50, "darkpulse", "sludgebomb", "flamethrower", "crunch"];
+teamMap["maxie"] = maxie;
+var archie = [155, "crunch", "firefang", "thunderfang", "icefang", 23, "airslash", "crosspoison", "darkpulse", "sludgebomb", 165, "gunkshot", "firepunch", "thunderpunch", "icepunch", 62, "nightslash", "liquidation", "icefang", "poisonfang", 173, "hydropump", "icebeam", "ancientpower", "thunder", 66, "waterfall", "crunch", "bodyslam", "dig"];
+teamMap["archie"] = archie;
+//Extra
+var wallace = [38, "hydropump", "icebeam", "poisonjab", "sludgebomb", 21, "dragonclaw", "hyperbeam", "surf", "earthquake", 59, "surf", "energyball", "gigadrain", "bodyslam", 92, "surf", "earthquake", "hyperbeam", "rockslide", 108, "hydropump", "brine", "blizzard", "doubleedge", 39, "surf", "icebeam", "irontail", "swift"];
+teamMap["wallace"] = wallace;
+var may = [117, "bodyslam", "aerialace", "gust", "magicalleaf", 59, "astonish", "tackle", "gigadrain", "surf", 99, "wingattack", "surf", "icebeam", "bodyslam", 7, "bravebird", "dig", "closecombat", "doubleedge", 6, "earthpower", "surf", "earthquake", "waterfall", 8, "x-scissor", "nightslash", "slash", "leafblade"];
+teamMap["may"] = may;
+
+//Gen 2
+//Champion
+var lance2 = [21, "tackle", "dragonpulse", "waterfall", "icefang", 82, "discharge", "dragonrush", "shockwave", "hyperbeam", 82, "discharge", "dragonrush", "shockwave", "hyperbeam", 81, "aerialace", "crunch", "rockslide", "thunderfang", 1, "shadowclaw", "airslash", "dragonclaw", "firefang", 82, "fireblast", "bodyslam", "outrage", "hyperbeam"];
+teamMap["lance (gen 2)"] = lance2;
+//Elite 4
+var will = [68, "tackle", "airslash", "shadowball", "psychic", 162, "slash", "astonish", "icepunch", "psychic", 78, "bodyslam", "energyball", "seedbomb", "psychic", 73, "shadowball", "brine", "bodyslam", "psychic", 68, "tackle", "airslash", "shadowball", "psychic", 101, "psychic", "shadowball", "bite", "dig"];
+teamMap["will"] = will;
+var koga = [163, "bodyslam", "x-scissor", "tackle", "gigadrain", 164, "slash", "gust", "psychic", "sludgebomb", 129, "doubleedge", "swift", "ironhead", "earthquake", 165, "bodyslam", "poisonfang", "sludgebomb", "darkpulse", 23, "bravebird", "tackle", "wingattack", "crosspoison", 76, "megahorn", "earthquake", "poisonjab", "slash"];
+teamMap["koga"] = koga;
+var bruno2 = [166, "nightslash", "tackle", "dig", "brickbreak", 167, "bodyslam", "karatechop", "closecombat", "slash", 168, "thunderpunch", "firepunch", "icepunch", "drainpunch", 169, "doubleedge", "earthquake", "stoneedge", "rockslide", 25, "rockslide", "bodyslam", "closecombat", "karatechop", 85, "rockslide", "ironhead", "earthquake", "dig"];
+teamMap["bruno (gen 2)"] = bruno2;
+var karen = [102, "dig", "shadowball", "faintattack", "darkpulse", 106, "gigadrain", "acid", "swift", "petaldance", 29, "shadowball", "darkpulse", "sludgebomb", "dig", 170, "tackle", "aerialace", "darkpulse", "faintattack", 50, "nightslash", "darkpulse", "flamethrower", "crunch", 74, "earthquake", "rockslide", "crunch", "icebeam"];
+teamMap["karen"] = karen;
+//Evil
+var giovanni2 = [76, "shadowclaw", "poisonjab", "doubleedge", "brickbreak", 30, "nightslash", "swift", "darkpulse", "shadowball", 51, "brickbreak", "rockslide", "icepunch", "earthquake", 77, "rockslide", "earthquake", "nightslash", "doubleedge", 84, "bodyslam", "stoneedge", "earthquake", "thunderpunch", 145, "earthquake", "rockslide", "tackle", "crunch"];
+teamMap["giovanni (gen 2)"] = giovanni2;
+//Extra
+var red = [146, "wildcharge", "irontail", "tackle", "thunderbolt", 80, "bodyslam", "brine", "blizzard", "psychic", 35, "shadowball", "crunch", "blizzard", "gigaimpact", 2, "energyball", "woodhammer", "sludgebomb", "earthquake", 1, "flamethrower", "flareblitz", "airslash", "dragonpulse", 0, "hydropump", "blizzard", "flashcannon", "focusblast"];
+teamMap["red"] = red;
+var silver = [41, "shadowclaw", "faintattack", "icywind", "metalclaw", 16, "discharge", "magnetbomb", "mirrorshot", "bodyslam", 29, "darkpulse", "psychic", "shadowball", "sludgebomb", 23, "aircutter", "bite", "shadowclaw", "crosspoison", 18, "psychic", "signalbeam", "swift", "focusblast", 3, "icefang", "slash", "aquatail", "crunch"];
+teamMap["silver"] = silver;
+
+//Gen 1
+//Champion
+var blue = [144, "wingattack", "doubleedge", "drillpeck", "knockoff", 18, "psybeam", "psychic", "shadowball", "signalbeam", 145, "doubleedge", "earthquake", "stoneedge", "megahorn", 134, "doubleedge", "wildcharge", "flareblitz", "flamethrower", 21, "outrage", "hydropump", "gigaimpact", "bounce", 0, "hydropump", "blizzard", "bite", "aurasphere"];
+teamMap["blue"] = blue;
+//Elite 4
+var lorelei = [174, "tackle", "aurorabeam", "psychic", "doubleedge", 137, "bodyslam", "slash", "aurorabeam", "dig", 73, "brine", "tackle", "shadowball", "psychic", 162, "slash", "icepunch", "bodyslam", "doubleedge", 80, "bodyslam", "shadowball", "hydropump", "blizzard", 159, "surf", "icebeam", "dig", "shadowclaw"];
+teamMap["lorelei"] = lorelei;
+var bruno1 = [166, "nightslash", "tackle", "dig", "brickbreak", 167, "bodyslam", "karatechop", "closecombat", "slash", 168, "thunderpunch", "firepunch", "icepunch", "drainpunch", 169, "doubleedge", "earthquake", "stoneedge", "rockslide", 25, "rockslide", "bodyslam", "closecombat", "karatechop", 85, "rockslide", "ironhead", "earthquake", "dig"];
+teamMap["bruno (gen 1)"] = bruno1;
+var agatha = [29, "faintattack", "shadowball", "psychic", "psybeam", 175, "bodyslam", "shadowball", "wingattack", "astonish", 176, "faintattack", "shadowball", "psychic", "psybeam", 177, "bite", "doubleedge", "slash", "poisonfang", 29, "faintattack", "shadowball", "sludgebomb", "psybeam", 176, "faintattack", "shadowball", "psychic", "psybeam"];
+teamMap["agatha"] = agatha;
+var lance1 = [21, "hydropump", "dragonclaw", "bodyslam", "hyperbeam", 178, "psychic", "slash", "dragonclaw", "hyperbeam", 178, "psychic", "slash", "dragonclaw", "hyperbeam", 81, "rockslide", "doubleedge", "bite", "hyperbeam", 82, "psychic", "slash", "icebeam", "hyperbeam", 82, "dragonclaw", "rockslide", "earthquake", "shadowball"];
+teamMap["lance (gen 1)"] = lance1;
+//Evil
+var giovanni1 = [145, "bodyslam", "dig", "doubleedge", "earthquake", 77, "bodyslam", "dig", "rockslide", "slash", 76, "tackle", "dig", "bodyslam", "poisonjab", 76, "tackle", "bodyslam", "poisonjab", "doubleedge", 145, "slash", "bodyslam", "earthquake", "doubleedge", 84, "earthquake", "rockslide", "surf", "megahorn"];
+teamMap["giovanni (gen 1)"] = giovanni1;
+//Extra
+var sabrina = [18, "psychic", "swift", "focusblast", "energyball", 96, "bodyslam", "shadowball", "thunder", "psychic", 164, "psybeam", "gust", "leechlife", "slash", 101, "dig", "psychic", "shadowball", "bite", 162, "psychic", "focusblast", "blizzard", "swift", 44, "stoneedge", "psychocut", "nightslash", "closecombat"];
+teamMap["sabrina"] = sabrina;
+
+document.getElementById("playerCur").onclick = function() {
+    if (document.getElementById("playerSelect").style.display == "flex") {
+        document.getElementById("playerSelect").style.display = "none";
+    } else {
+        if (formulateSquadrine()) {
+            document.getElementById("custom").hidden = false;
+        } else {
+            document.getElementById("custom").hidden = true;
+        }
+        document.getElementById("playerSelect").style.display = "flex";
+    }
+}
+
+document.getElementById("enemyCur").onclick = function() {
+    if (document.getElementById("enemySelect").style.display == "flex") {
+        document.getElementById("enemySelect").style.display = "none";
+    } else {
+        if (formulateSquadrine()) {
+            document.getElementById("custom2").hidden = false;
+        } else {
+            document.getElementById("custom2").hidden = true;
+        }
+        document.getElementById("enemySelect").style.display = "flex";
+    }
+}
+
+var pOptions = document.getElementById("playerOptions");
+for (let i = 0; i < pOptions.childNodes.length; i++) {
+    if (pOptions.childNodes[i].tagName == "DIV") {
+        pOptions.childNodes[i].onclick = function() {
+            document.getElementById("playerSelect").style.display = "none";
+            document.getElementById("playerCur").innerHTML = this.innerHTML + "<div style='position: absolute; right: 0;'>▾</div>";
+        }
+    }
+}
+
+var eOptions = document.getElementById("enemyOptions");
+for (let i = 0; i < eOptions.childNodes.length; i++) {
+    if (eOptions.childNodes[i].tagName == "DIV") {
+        eOptions.childNodes[i].onclick = function() {
+            document.getElementById("enemySelect").style.display = "none";
+            document.getElementById("enemyCur").innerHTML = this.innerHTML + "<div style='position: absolute; right: 0;'>▾</div>";
+        }
+    }
+}
+
+document.getElementById("build").onclick = async function() {
+    document.getElementById("playerText").hidden = true;
+    document.getElementById("enemyText").hidden = true;
+    document.getElementById("vs").hidden = true;
+    document.getElementById("start").hidden = true;
+    document.getElementById("build").hidden = true;
+    document.getElementById("textPortion").hidden = true;
+    document.getElementById("playerCur").style.display = "none";
+    document.getElementById("enemyCur").style.display = "none";
+    document.getElementById("teamParent").style.display = "block";
+    document.getElementById("done").hidden = false;
+    document.getElementById("playerSelect").style.display = "none";
+    document.getElementById("enemySelect").style.display = "none";
+    document.getElementById("playerOptions").hidden = true;
+    document.getElementById("enemyOptions").hidden = true;
+}
+
+
+document.getElementById("done").onclick = function() {
+    if (formulateSquadrine()) {
+        document.getElementById("playerCur").innerHTML = "<span>Custom Team</span><img class='optionImage' style='margin-left: 10px;' src='images/pokemon/Charizard.png'><div style='position: absolute; right: 0;'>▾</div>";
+        document.getElementById("enemyCur").innerHTML = "<span>Custom Team</span><img class='optionImage' style='margin-left: 10px;' src='images/pokemon/Charizard.png'><div style='position: absolute; right: 0;'>▾</div>";
+    }
+    document.getElementById("playerText").hidden = false;
+    document.getElementById("enemyText").hidden = false;
+    document.getElementById("vs").hidden = false;
+    document.getElementById("start").hidden = false;
+    document.getElementById("build").hidden = false;
+    document.getElementById("textPortion").hidden = false;
+    document.getElementById("playerCur").style.display = "flex";
+    document.getElementById("enemyCur").style.display = "flex";
+    document.getElementById("teamParent").style.display = "none";
+    document.getElementById("done").hidden = true;
+    document.getElementById("playerOptions").hidden = false;
+    document.getElementById("enemyOptions").hidden = false;
+}
+
+document.getElementById("check").onanimationend = function() {
+    this.hidden = true;
+}
