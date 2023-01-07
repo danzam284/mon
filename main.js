d3e7fd6b72beb26bfb281fd7dfa38874def6401b
Daniel Zamloot <45713834+danzam284@users.noreply.github.com>
2023-01-06 20:11:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -505,3 +505,19 @@ document.addEventListener('visibilitychange', e=> {
         switching = false;
     }
 });
+
+function isMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
